289101e760aaa99f16ed2cf882f291842dd6e245
Sophia Li <sl.sophiayli@gmail.com>
2025-08-23 21:53:07 -0400

    Load models & textures with draco and gltf

--- src/main.js
+++ src/main.js
@@ -1,1 +1,116 @@
 import './style.scss'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const canvas = document.querySelector("#experience-canvas")
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+// Loaders
+const textureLoader = new THREE.TextureLoader();
+
+// Model loader
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( '/draco/' );
+
+const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
+
+const textureMap = {
+  First: {
+    day: "/textures/TextureSet1.webp"
+  },
+  Second: {
+    day: "/textures/TextureSet2.webp"
+  },
+  Third: {
+    day: "/textures/TextureSet3.webp"
+  }
+};
+
+const loadedTextures = {
+  day: {}
+};
+
+Object.entries(textureMap).forEach(([key, paths]) => {
+  const dayTexture = textureLoader.load(paths.day);
+  dayTexture.flipY = false;
+  loadedTextures.day[key] = dayTexture;
+
+  // Night textures TBD
+});
+
+loader.load("/models/Room_Portfolio.glb", (glb) => {
+
+  glb.scene.traverse(child => {
+    if (child.isMesh) {
+      Object.keys(textureMap).forEach((key) => {
+        if (child.name.includes(key)) {
+          const material = new THREE.MeshBasicMaterial({
+            map: loadedTextures.day[key],
+          });
+
+          child.material = material;
+        }
+      });
+    }
+
+    scene.add(glb.scene);
+  })
+});
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 );
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+renderer.toneMapping = THREE.NoToneMapping;
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.update();
+
+// Event listeners
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+function animate() {
+
+}
+
+const render = () => {
+  controls.update();
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+
+  window.requestAnimationFrame(render);
+}
+
+render()
